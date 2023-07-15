fa0893c30adea2d31c87a4287183d71bab2910ac
RalfiSlask <112242026+RalfiSlask@users.noreply.github.com>
2023-07-15 21:05:16 +0200

    commit

---
+++ kanban-task/src/App.js
@@ -0,0 +1,11 @@
+import ButtonAddNewColumn from "./ButtonAddNewColumn";
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAddNewColumn />
+    </div>
+  );
+}
+
+export default App;
---
+++ kanban-task/src/ButtonAddNewColumn.js
@@ -0,0 +1,9 @@
+
+
+const ButtonAddNewColumn = () => {
+  return (
+    <button className="bg-red-300">+Add New Column</button>
+  )
+}
+
+export default ButtonAddNewColumn
